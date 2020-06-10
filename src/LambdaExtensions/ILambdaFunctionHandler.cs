b66877a1aea6e3d7d7b9a1ca2ee8c9a8616ce07b
Indika Udagedara <indikau@openpay.com.au>
2020-06-10 22:55:34 +1000

    Cleanup

--- src/LambdaExtensions/ILambdaFunctionHandler.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace LambdaExtensions
-{
-    public interface ILambdaFunctionHandler<TRequest, TResponse>
-    {
-        Task<TResponse> HandleAsync(TRequest request);
-    }
-}
