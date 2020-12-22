b9ebac5ef6da4b78accd22fd41922e01b373f322
Indika Udagedara <indika.udagedara@gmail.com>
2020-12-22 23:03:22 +1100

    Bug fix

--- Build.ps1
+++ Build.ps1
@@ -1,5 +1,5 @@
 rm -r publish/
 dotnet build -o publish/
 pushd publish
-dotnet nuget push *.nupkg -k oy2oxerznwpsmnm7bbequqdbmdd3cgauxgtivvgzpjkeh4 -s https://api.nuget.org/v3/index.json
+dotnet nuget push *.nupkg -k x -s https://api.nuget.org/v3/index.json
 popd
